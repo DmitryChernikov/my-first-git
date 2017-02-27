a612699bd9bf5ab8ca62e0f602b74b2598e08e21
dmitriy <dmitrychernikov@mail.ru>
2017-02-28 00:02:48 +0300

    3

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,10 @@
-﻿
+﻿http://localhost:54542/../styles/style.css
+
+
+    $(".task-row").click(function () {
+        $(this).toggleClass("chekon");
+    });
+
+    $(".close").click(function () {
+        $(this).parent().remove();
+    })
